ff2b97916a5f1f9df16bc53bae2c0402b3de16fe
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-15 14:41:06 -0700

    add posts

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const db = require('./models');
 const app = express();
 
-//Routers
 
+//be able to parse json reponse
+app.use(express.json());
+
+//Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { Posts } = require("../models");
 
-router.get();
 
-router.post();
+router.get('/', (req, res) => {
+    res.send("Hello to your mom!");
+});
 
-module.export = router;
+router.post('/', async (req, res) => {
+    const post = req.body;
+
+    //inserts a row into database
+    await Posts.create(post);
+
+    //return same data that was sent
+    res.json(post);
+});
+
+module.exports = router;
