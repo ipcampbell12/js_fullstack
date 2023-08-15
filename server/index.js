c4da5cbcdc96817df958975154e7a396944b9d2d
ipcampbell12 <ipcampbell12@gmail.com>
2023-08-15 14:07:22 -0700

    databse

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const db = require('./models');
 //initialize an express instace
 const app = express();
 
-
-app.listen(3001, () => {
-    console.log("Server is running on port 3001")
+//when we start API, and check to see if all tables have been created
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server is running on port 3001");
+    });
 });
+
